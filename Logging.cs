e8435217e97bba4f141cfb170f0d9791b9dcb0e0
deba793 <debasisrout793@gmail.com>
2023-08-04 15:57:46 +0530

    Assignment-23

---
+++ Logging.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace MySharedLibrary
+{
+    public class Logging
+    {
+
+        private int id;
+
+        public int ID
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        private string password;
+
+        public string Password
+        {
+            get { return password; }
+            set { password = value; }
+        }
+    }
+}
+    
+
---
+++ LoginOpearation.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySharedLibrary
+{
+    internal class LoginOpearation
+    {
+        static List<Logging> list = new List<Logging>()
+        {
+            new Logging {ID = 2567455,Password = "b1434"},
+            new Logging {ID = 2567456,Password = "b15635"},
+            new Logging {ID = 2567457,Password = "b123896"},
+            new Logging {ID = 2567458,Password = "b124537"},
+            new Logging {ID = 2567459,Password = "b127838"},
+        };
+        public static void login()
+        {
+            Logging loginDetails = new Logging();
+            Console.WriteLine("Enter ID: ");
+            int id = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Password: ");
+            string password = Console.ReadLine();
+            Logging l = list.SingleOrDefault(e => e.ID == id && e.Password == password);
+            if (l != null)
+            {
+                Console.WriteLine("Login Successfull!!");
+            }
+            else
+            {
+                Console.WriteLine("Invalid Details!!");
+            }
+
+        }
+    }
+}
